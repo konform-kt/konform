64091450899143901b61901c398fa6dc8a5b46d9
Niklas Lochschmidt <niklas.lochschmidt@flincteam.org>
2018-04-17 14:33:17 +0200

    Remove expect/actual ValidationBuilder
    Using a local JvmName annotation it is possible to remove the almost
    identical ValidationBuilders from the jvm and js projects. In Kotlin
    1.3 the @JvmName annotation is likely to be usable in the common code.

---
+++ js/src/main/kotlin/io/konform/validation/JvmName.kt
@@ -0,0 +1,4 @@
+package io.konform.validation
+
+// FIXME: Remove once JvmName is available in JS projects
+actual annotation class JvmName actual constructor(actual val name: String)
---
+++ jvm/src/main/kotlin/io/konform/validation/JvmName.kt
@@ -0,0 +1,4 @@
+package io.konform.validation
+
+// FIXME: Remove once JvmName is available in JS projects
+actual typealias JvmName = kotlin.jvm.JvmName
