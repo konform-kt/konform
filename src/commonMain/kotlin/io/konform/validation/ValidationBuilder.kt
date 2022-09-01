994d1c190f15101acf143b6aac3b916a80ccf866
Niklas Lochschmidt <nlochschmidt@gmail.com>
2022-09-01 11:29:49 +0200

    Allow validation of single values
    The ValidationResult became covariant, but that makes sense as a
    ValidationResult for Object should be able to contain any value that is
    a subclass of object.

--- src/commonMain/kotlin/io/konform/validation/ValidationBuilder.kt
+++ src/commonMain/kotlin/io/konform/validation/ValidationBuilder.kt
@@ -3,6 +3,8 @@ package io.konform.validation
 import io.konform.validation.internal.ArrayValidation
 import io.konform.validation.internal.IterableValidation
 import io.konform.validation.internal.MapValidation
+import io.konform.validation.internal.OptionalValidation
+import io.konform.validation.internal.RequiredValidation
 import io.konform.validation.internal.ValidationBuilderImpl
 import kotlin.jvm.JvmName
 import kotlin.reflect.KProperty1
@@ -29,6 +31,18 @@ abstract class ValidationBuilder<T> {
     abstract infix fun <R> KProperty1<T, R?>.required(init: ValidationBuilder<R>.() -> Unit)
     abstract fun run(validation: Validation<T>)
     abstract val <R> KProperty1<T, R>.has: ValidationBuilder<R>
+}
+
+fun <T : Any> ValidationBuilder<T?>.ifPresent(init: ValidationBuilder<T>.() -> Unit) {
+    val builder = ValidationBuilderImpl<T>()
+    init(builder)
+    run(OptionalValidation(builder.build()))
+}
+
+fun <T : Any> ValidationBuilder<T?>.required(init: ValidationBuilder<T>.() -> Unit) {
+    val builder = ValidationBuilderImpl<T>()
+    init(builder)
+    run(RequiredValidation(builder.build()))
 }
 
 @JvmName("onEachIterable")
