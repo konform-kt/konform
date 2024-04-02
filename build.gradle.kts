ca9eb5b0c71de186f52838bab34bdd6e83591efb
David Hoepelman <dhoepelman@flexport.com>
2024-04-02 13:29:07 +0200

    Do a real release, add PUBLISHING.md

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.cli.common.toBooleanLenient
 import org.jetbrains.kotlin.config.JvmTarget
 
-val projectVersion = "0.5.0"
+val projectVersion = "0.5.0-SNAPSHOT"
 val projectName = "konform"
 val projectGroup = "io.konform"
 val projectDesc = "Konform: Portable validations for Kotlin"
