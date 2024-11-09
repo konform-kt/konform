8a5066e472921d4f2a6f36dcdc75962d6e8bf527
David Hoepelman <dhoepelman@flexport.com>
2024-11-09 20:09:09 +0100

    Move Constraint to its own file

--- src/commonMain/kotlin/io/konform/validation/Validation.kt
+++ src/commonMain/kotlin/io/konform/validation/Validation.kt
@@ -12,9 +12,3 @@ public interface Validation<in T> {
 
     public operator fun invoke(value: T): ValidationResult<@UnsafeVariance T> = validate(value)
 }
-
-public class Constraint<in R> internal constructor(
-    public val hint: String,
-    public val templateValues: List<String>,
-    public val test: (R) -> Boolean,
-)
