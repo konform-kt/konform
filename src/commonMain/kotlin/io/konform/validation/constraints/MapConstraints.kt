dafa700dc19a5a8946eb5cac276957dd405324c3
David Hoepelman <dhoepelman@flexport.com>
2024-11-13 08:11:23 +0100

    Add fixes and deprecation

--- src/commonMain/kotlin/io/konform/validation/constraints/MapConstraints.kt
+++ src/commonMain/kotlin/io/konform/validation/constraints/MapConstraints.kt
@@ -3,23 +3,23 @@ package io.konform.validation.constraints
 import io.konform.validation.Constraint
 import io.konform.validation.ValidationBuilder
 
-public fun <K, V> ValidationBuilder<Map<K, V>>.minItems(minSize: Int): Constraint<Map<K, V>> =
+public fun <T : Map<*, *>> ValidationBuilder<T>.minItems(minSize: Int): Constraint<T> =
     addConstraint("must have at least {0} items", minSize.toString()) {
         it.count() >= minSize
     }
 
-public fun <K, V> ValidationBuilder<Map<K, V>>.maxItems(maxSize: Int): Constraint<Map<K, V>> =
+public fun <T : Map<*, *>> ValidationBuilder<T>.maxItems(maxSize: Int): Constraint<T> =
     addConstraint("must have at most {0} items", maxSize.toString()) {
         it.count() <= maxSize
     }
 
-public fun <K, V> ValidationBuilder<Map<K, V>>.minProperties(minSize: Int): Constraint<Map<K, V>> =
+public fun <T : Map<*, *>> ValidationBuilder<T>.minProperties(minSize: Int): Constraint<T> =
     minItems(minSize) hint "must have at least {0} properties"
 
-public fun <K, V> ValidationBuilder<Map<K, V>>.maxProperties(maxSize: Int): Constraint<Map<K, V>> =
+public fun <T : Map<*, *>> ValidationBuilder<T>.maxProperties(maxSize: Int): Constraint<T> =
     maxItems(maxSize) hint "must have at most {0} properties"
 
-public fun <K, V> ValidationBuilder<Map<K, V>>.uniqueItems(unique: Boolean = true): Constraint<Map<K, V>> =
+public fun <T : Map<*, *>> ValidationBuilder<T>.uniqueItems(unique: Boolean = true): Constraint<T> =
     addConstraint("all items must be unique") {
         !unique || it.values.distinct().count() == it.count()
     }
