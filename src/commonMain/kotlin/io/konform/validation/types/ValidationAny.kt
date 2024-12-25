3d5f3b63cb0b81869dd3374ae1c6f11b1091ec25
David Hoepelman <dhoepelman@flexport.com>
2024-12-25 20:52:41 +0100

    wip

--- src/commonMain/kotlin/io/konform/validation/types/ValidationAny.kt
+++ src/commonMain/kotlin/io/konform/validation/types/ValidationAny.kt
@@ -4,34 +4,31 @@ import io.konform.validation.Invalid
 import io.konform.validation.Valid
 import io.konform.validation.Validation
 import io.konform.validation.ValidationResult
-
-private inline fun <T> gatherInvalid(
-    validations: List<Validation<T>>,
-    value: T,
-): List<Invalid> {
-    val errors = mutableListOf<Invalid>()
-    for (validation in validations) {
-        val result = validation.validate(value)
-        if (result is Invalid) errors += result
-    }
-    return errors
-}
+import io.konform.validation.flattenNotEmpty
+import io.konform.validation.flattenOrValid
 
 public class ValidationAny<T>(
     private val validations: List<Validation<T>>,
-    private val createHint: (List<Invalid>) -> String = ::defaultCreateHint,
+    private val aggregateInvalidResults: (List<Invalid>) -> Invalid = List<Invalid>::flattenNotEmpty,
 ) : Validation<T> {
     override fun validate(value: T): ValidationResult<T> {
-        val invalids = gatherInvalid(validations, value)
-        return if (invalids.size < validations.size) {
-            Valid(value)
-        } else {
-            Invalid()
+        val errors = mutableListOf<Invalid>()
+        for (validation in validations) {
+            when (val result = validation.validate(value)) {
+                // We only need 1 validation to succeed the "any" validation
+                is Valid -> return result
+                is Invalid -> errors + result
+            }
         }
+        return errors.flattenOrValid(value)
     }
 
+    override fun toString(): String = "ValidationAny(validation=$validations)"
+
     private companion object {
-        private fun defaultCreateHint(invalids: List<Invalid>): String =
-            "all validations failed: ${invalids.flatMap { invalid -> invalid.errors.map { it.message } }}"
+        private fun defaultAggregateInvalidResults(invalids: List<Invalid>): Invalid {
+            val combinedErrors =
+                "all validations failed: ${invalids.flatMap { invalid -> invalid.errors.map { it.message } }}"
+        }
     }
 }
